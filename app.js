dab5b4fe2b8d083345b14e95cc878a50e055a53e
Miguel Alquinta <miguel.alquinta@gmail.com>
2025-08-11 11:53:29 -0400

    Se crean todas las validaciones para el ingreso de Amigos a la lista

--- app.js
+++ app.js
@@ -3,19 +3,28 @@ let arrayAmigos = [];
 let nuevoElemento = document.createElement('li');
 
 function agregarAmigo (){
-    valorAmigo = document.getElementById('amigo').value.trim();    
-    //Valida que no se ingrese un nombre de amigo en blanco
+    valorAmigo = document.getElementById('amigo').value.trim(); 
+    //Limpia los espacios en blanco extras
+    valorAmigo = valorAmigo.replace(/\s+/g, ' ').trim();  
+    //Valida que se ingrese un nombre valido
     if (valorAmigo === "" || !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(valorAmigo)) {
         alert('Por favor ingrese un nombre valido');
         limpiarTexto();
     } 
     else {
-    alert('Se ingresó amigo');
-    agregarLista();
-    limpiarTexto();
-        }
+        //revisa que nombre amigo no exista en la lista, si esta escrito con mayusculas o minusculas igual lo detecta
+        if (arrayAmigos.some(nombre => nombre.toLowerCase() === valorAmigo.toLowerCase())){
+            alert('Nombre ya existe en la lista, ingrese otro nombre')
+            limpiarTexto();
+             }else{
+                alert('Se ingresó amigo');
+                agregarLista();
+                limpiarTexto();
+                 }
+         }
 }
 
+//Llena lista con nombre y agrega nombre al arreglo
 function agregarLista(){
     arrayAmigos.push(valorAmigo);
     nuevoElemento = document.createElement('li');
@@ -24,6 +33,7 @@ function agregarLista(){
     console.log(arrayAmigos);
 }
 
+//limpia el texto
 function limpiarTexto (){
      document.getElementById('amigo').value = "";
 }
