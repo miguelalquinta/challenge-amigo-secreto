4c46bb6808f0edb0c04e90bb7ad99adb6fab338d
Miguel Alquinta <miguel.alquinta@gmail.com>
2025-08-10 22:48:41 -0400

    Se programa condiciones para que no acepte valores nulos y números

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let valorAmigo = "";
+let arrayAmigos = [];
+let nuevoElemento = document.createElement('li');
+
+function agregarAmigo (){
+    valorAmigo = document.getElementById('amigo').value.trim();    
+    //Valida que no se ingrese un nombre de amigo en blanco
+    if (valorAmigo === "" || !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(valorAmigo)) {
+        alert('Por favor ingrese un nombre valido');
+        limpiarTexto();
+    } 
+    else {
+    alert('Se ingresó amigo');
+    agregarLista();
+    limpiarTexto();
+        }
+}
+
+function agregarLista(){
+    arrayAmigos.push(valorAmigo);
+    nuevoElemento = document.createElement('li');
+    nuevoElemento.textContent = valorAmigo;
+    document.getElementById('listaAmigos').appendChild(nuevoElemento);
+    console.log(arrayAmigos);
+}
+
+function limpiarTexto (){
+     document.getElementById('amigo').value = "";
+}
